b804fa1743b2407ebebdafc71a0690b56c70d7ed
jangh-lee <72970232+jangh-lee@users.noreply.github.com>
2022-06-08 17:03:57 +0900

    Update 윤년.py

--- 백준/Bronze/2753. 윤년/윤년.py
+++ 백준/Bronze/2753. 윤년/윤년.py
@@ -1,4 +1,5 @@
 year = int(input())
+
 if year % 4 == 0:
     if year % 100 !=0 or year % 400==0:
       print("1")
@@ -6,3 +7,6 @@ if year % 4 == 0:
       print("0")
 else:
   print("0")
+
+# 배운점 1. 중첩 if 문의 사용
+# 배운점 2. if 조건에 or,and 사용시 대문자 불가능 및 띄어쓰기 준수
