e71b43cab47e815580b919edb36aca04de4f82ab
jangh-lee <72970232+jangh-lee@users.noreply.github.com>
2022-06-09 21:55:56 +0900

    Update A＋B － 7.py

--- 백준/Bronze/11021. A＋B － 7/A＋B － 7.py
+++ 백준/Bronze/11021. A＋B － 7/A＋B － 7.py
@@ -3,3 +3,7 @@ x = int(input())
 for i in range(1, x+1):
     a,b = map(int,input().split())
     print("Case #{}: {}".format(i,a+b))
+
+    
+    
+# print 문의 활용 : https://blackhippo.tistory.com/entry/Python-print%EB%AC%B8%EC%9C%BC%EB%A1%9C-%EB%AC%B8%EC%9E%90%EC%97%B4-%ED%91%9C%ED%98%84%ED%95%98%EB%8A%94-%EC%97%AC%EB%9F%AC%EA%B0%80%EC%A7%80-%EB%B0%A9%EB%B2%95
